636cb6b876015a01b3750817d915dfed870fe890
ZEP <zephyr.lathuy.zpl@gmail.com>
2025-05-16 21:21:22 +0200

    retraivaille des text de vision

--- porfolio1/src/sections/vision/vision.jsx
+++ porfolio1/src/sections/vision/vision.jsx
@@ -1,5 +1,5 @@
 import styles from '../../assets/styles/vision.module.css';
-import React, { use, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
@@ -16,7 +16,7 @@ export default function Vision(){
         const visionSteps = visionStepRef.current;
 
         gsap.to(visionSteps, {
-        y: () => -(visionSteps.scrollHeight - window.innerHeight),
+        y: () => -(visionSteps.scrollHeight),
         ease: "none",
         scrollTrigger: {
             trigger: sectionVisionRef.current,
@@ -42,43 +42,55 @@ export default function Vision(){
                 <div className={styles.visionHeader}>
                     <p>
                         <span className={styles.span1}>
-                            I provide usability-focused development across various areas, styling, CMS development, motion design, and UI/UX optimization.
+                            I design and develop digital products that prioritize usability, speed, and seamless interaction.
+                            From CMS-driven websites to refined UI animations, 
                         </span><br />
                         <span className={styles.span2}>
-                          I also prioritize performance improvements and accessibility to deliver a smooth and intuitive web experience.   
+                            I focus on creating accessible and elegant experiences that are both visually engaging and technically sound.
                         </span>
                     </p>
                 </div>
                 <div ref={visionStepRef} className={styles.visionStep}>
                     <div className={styles.step}>
-                        <div className={styles.stepTitle}>Direction</div>
+                        <div className={styles.stepTitle}>Discovery phase</div>
                         <div className={styles.stepContent}>
                             <p>
-                                In my opinion, deep understanding of the brief and client is a must in the starting point. Determining project goals and functional specifications, knowing competitors and target audiences, making researches will be taken at this stage in order to find a precise combination between Typography, Color, Motion and brand assets. For me, this process is the foundation for solving problems and discovering a right direction for the perfect design.
+                                For me, every successful project starts with a deep understanding of the client and their vision.
+                                At this stage, I focus on defining clear goals and technical requirements, researching the competitive landscape, and identifying the target audience.
+                                This discovery phase allows me to align strategy with creativity—to find the right balance between typography, color, motion, and brand identity.
+                                It’s a critical foundation that helps uncover meaningful directions and design solutions that truly resonate.
                             </p>
                         </div>
                     </div>
                     <div className={styles.step}>
-                        <div className={styles.stepTitle}>Design</div>
+                        <div className={styles.stepTitle}>Exploring Visual Directions</div>
                         <div className={styles.stepContent}>
                             <p>
-                                After collecting a right mood and tone in the Direction stage. My mission at this point is connecting the dots and making various versions to find the proper design. My goal is always about creating an exprience that not only emphasises the characteristics of each project but also elegant and satisfied to use. To get that, I have to make sure to work closely with clients and users to get helpful feedbacks and insights for the evolution.
+                                Once the right mood and tone have been defined during the discovery phase, I move on to exploring visual directions.
+                                At this stage, my mission is to connect the dots translating strategy into design by experimenting with different concepts and layouts.
+                                My goal is to craft an experience that not only reflects the unique identity of each project, but is also elegant, functional, and genuinely enjoyable to use.
+                                This process is always collaborative: I work closely with clients and users to gather meaningful feedback and refine the design into its most effective form.
                             </p>
                         </div>
                     </div>
                     <div className={styles.step}>
-                        <div className={styles.stepTitle}>Interaction</div>
+                        <div className={styles.stepTitle}>Crafting the Experience with Motion</div>
                         <div className={styles.stepContent}>
                             <p>
-                                I belive Motion and interaction  are the key elements that help to bring satisfaction to the audiences the best possible way. A small subtle motion or microinteraction can push the experience to a whole new level effortlessly. I love seeing people get "wow" and keep toying with those interactions. That’s the reason why I often spend a lot of time and effort for this stage on each project.
+                                I believe that motion and interaction are essential to creating truly engaging digital experiences.
+                                Even the smallest micro interaction a subtle hover effect, a smooth transition, or a gentle scroll animation can elevate the interface and bring it to life.
+                                These details not only enhance usability, but also create emotional responses that make users want to explore and engage more deeply.
+                                That's why I devote significant time and attention to interaction design in every project: it's where function meets delight.
                             </p>
                         </div>
                     </div>
                     <div className={styles.step}>
-                        <div className={styles.stepTitle}>Development</div>
+                        <div className={styles.stepTitle}>Final Product Execution</div>
                         <div className={styles.stepContent}>
                             <p>
-                                My job is not over when it's still on Figma, or in prototype, design is the final product and as a designer I have to make sure the final product must be absolutely pixel-perfect with the design, from every single element like spacing, composition, typography,... to motion, interaction and of course functionality, responsive devices.
+                                My role extends far beyond the design phase in Figma or prototyping. As a designer, I ensure the final product is meticulously executed, aligning perfectly with the intended design vision.
+                                Every detail—from spacing and typography to motion, interaction, and responsiveness—must be flawlessly implemented across all devices and platforms.
+                                I am committed to delivering a seamless, high-performance user experience, ensuring that the design is not only visually accurate but also fully functional and intuitive.
                             </p> 
                         </div>
                     </div>
